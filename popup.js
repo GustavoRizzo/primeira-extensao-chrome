87b908bd7abf7ab83d1e47c4d6bb24851cb5321d
Gustavo <grizzo.albu@gmail.com>
2020-12-21 19:56:21 -0300

    hello word em ingles e espanhol

---
+++ popup.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById('btn').addEventListener("click", function(){
+    document.getElementById('body').innerHTML = "<h1 class='h1'>Hola Mundo! 🌎</h1>";
+    alert('Olá from background');
+    console.log('Olá from background');
+});
